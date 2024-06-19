01ec9d64a17b5f25de9af09d91eed41d295768aa
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-06-19 13:33:12 +0000

    added post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-harrisong5-blog2-k3m4sekswcu.ws.codeinstitute-ide.net','.herokuapp.com']
 
@@ -81,6 +81,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "https://*8000-harrisong5-blog2-k3m4sekswcu.ws.codeinstitute-ide.net"
+]
 
 
 # Password validation
