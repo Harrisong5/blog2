a914f77546eaab9e270da88d514cc3c8ff357d56
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-06-19 14:11:28 +0000

    added excerpt to models code

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,4 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
