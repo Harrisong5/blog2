15a8018fcc3adb933d6092381578bb80eb4d5983
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-06-19 15:28:18 +0000

    meta class in comments model

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
     
     class Meta:
         ordering = ["-created_on"]
-        
+
     def __str__(self):
         return f"The title of this post is {self.title}"
 
@@ -30,4 +30,10 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
 
